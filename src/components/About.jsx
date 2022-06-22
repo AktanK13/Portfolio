fe1bb4aeaeec917bdef02de7e3ecbe1b9d305a4f
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-06-22 20:08:46 +0600

    add readme file

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi. I'm Aktan, nice to meet you. Please take a look around.</p>
+            </div>
+            <div>
+              <p>I am passionate about building excellent software that improves
+              the lives of those around me. I specialize in creating software
+              on React, I completed my studies as a front-end developer at the Codify Academy, did an internship there.
+              What would you do if you had
+              a software expert available at your fingertips?</p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
