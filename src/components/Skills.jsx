51dd8a6e482e57919bb98a2feba4de9732236a8d
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-06-25 14:03:47 +0600

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -41,8 +41,7 @@ const Skills = () => {
                   <p className='my-4'>GITHUB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                  <p className='my-4'>NODE JS</p>
+                  
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
