fb902a4ea811cab25a49301029facd4384796c8d
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-07-30 01:39:45 +0600

    first commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import buhi from '../assets/BUHI.png'
 
 const Work = () => {
   return (
@@ -16,23 +17,18 @@ const Work = () => {
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url()` }}
+            style={{ backgroundImage: `url(${buhi})`, backgroundSize: 'cover' }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
+            <div className='opacity-0 group-hover:opacity-80'>
               <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://github.com/BUHi-kg/BUHi-kg'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
+                    GitHub repository
                   </button>
                 </a>
               </div>
